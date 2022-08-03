f3279977bff422e121fbd27558fe594383cc9e5a
krso1002 <56928680+krso1002@users.noreply.github.com>
2022-08-03 15:45:04 +0200

    Rydding av kode

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from cmath import *
 from textwrap import fill
 from tkinter import *
-from turtle import window_height
 
 tk = Tk()
 tk.title("Golf")
@@ -15,7 +14,13 @@ canvas.pack()
 #set background color to green
 canvas.create_rectangle(0, 0, WIDTH+20, 450, fill="green")
 
-#top right corner of screen
+#distance markers
+canvas.create_rectangle(215, 400, 225, 450, fill="blue")
+canvas.create_rectangle(415, 400, 425, 450, fill="red")
+canvas.create_rectangle(615, 400, 625, 450, fill="orange")
+canvas.create_rectangle(815, 400, 825, 450, fill="purple")
+
+#distance markers explaination
 canvas.create_rectangle(800, 0, 925, 50, fill="blue")
 label = Label(canvas, text="50m", font=("Arial", 20), bg="blue")
 label.place(x=835, y=10)
@@ -28,13 +33,6 @@ label.place(x=835, y=60)
 canvas.create_rectangle(925, 50, 1050, 100, fill="purple")
 label = Label(canvas, text="200m", font=("Arial", 20), bg="purple")
 label.place(x=960, y=60)
-
-#avstandspinner
-canvas.create_rectangle(215, 400, 225, 450, fill="blue")
-canvas.create_rectangle(415, 400, 425, 450, fill="red")
-canvas.create_rectangle(615, 400, 625, 450, fill="orange")
-canvas.create_rectangle(815, 400, 825, 450, fill="purple")
-
 
 #create ball
 ball = canvas.create_oval(10, 430, 30, 450, fill="white")
@@ -58,18 +56,14 @@ def MoveBall():
     canvas.move(ball, xSpeed, ySpeed)
     (x1, y1, x2, y2) = canvas.coords(ball)
 
-    #create line that follows ball
-    canvas.create_oval(x1+9, y1+9, x2-9, y2-9, fill="white")
-
     if y2 >= 450:
         return
     
     canvas.after(30, MoveBall)
-    canvas.after(150, airResistance)
+    canvas.after(150, AirResistance)
     canvas.after(30, angle)
-    
-def airResistance():
-    label['text'] = "Air Resistance"
+
+def AirResistance():
     global xSpeed, ySpeed
 
     #air resistance for x-axis
@@ -81,90 +75,88 @@ def airResistance():
     airResCoeffY = dragCoeff * ((airDensity * ySpeed ** 2) / 2) * dragArea
     dragAccY = (weight - airResCoeffY) / mass
     ySpeed = ySpeed + dragAccY
-    return
 
 def angle():
     #calculate angle of ball trajectory
     if xSpeed > ySpeed:
         angle = atan(ySpeed/xSpeed) * 180 / pi
         #print(angle)
-        
 
 label = Label(tk, text="Select Club", font=("Arial", 30))
 label.pack()
 
 def driver():
-    label['text'] = "Driver (15 degrees)"
+    label['text'] = "Driver"
     global xSpeed, ySpeed
     xSpeed = 113
     ySpeed = -31
     
-def fiveIron():
+def FiveIron():
     label['text'] = "5 Iron"
     global xSpeed, ySpeed
     xSpeed = 72
     ySpeed = -32
 
-def sixIron():
+def SixIron():
     label['text'] = "6 Iron"
     global xSpeed, ySpeed
     xSpeed = 64
     ySpeed = -33
 
-def sevenIron():
+def SevenIron():
     label['text'] = "7 Iron"
     global xSpeed, ySpeed
     xSpeed = 56.8
     ySpeed = -33.6
 
-def eightIron():
+def EightIron():
     label['text'] = "8 Iron"
     global xSpeed, ySpeed
     xSpeed = 50
     ySpeed = -35
 
-def nineIron():
+def NineIron():
     label['text'] = "9 Iron"
     global xSpeed, ySpeed
     xSpeed = 44
     ySpeed = -36
 
-def pitchingWedge():
+def PitchingWedge():
     label['text'] = "Pitching Wedge"
     global xSpeed, ySpeed
     xSpeed = 38
     ySpeed = -37
 
-def sandWedge():
+def SandWedge():
     label['text'] = "Sand Wedge"
     global xSpeed, ySpeed
     xSpeed = 24
     ySpeed = -36.5
 
-def threeWood():
+def ThreeWood():
     label['text'] = "Fairway Wood"
     global xSpeed, ySpeed
     xSpeed = 100
     ySpeed = -31
 
-def resetBall():
+def ResetBall():
+    label['text'] = "Select Club"
     canvas.coords(ball, 10, 430, 30, 450)
     global xSpeed, ySpeed
     xSpeed = 0
     ySpeed = 0
-    return
 
 #create button for every club
 button = Button(tk, text="Driver", command=driver)
-button2 = Button(tk, text="Wood", command=threeWood)
-button3 = Button(tk, text="5 iron", command=fiveIron)
-button4 = Button(tk, text="6 iron", command=sixIron)
-button5 = Button(tk, text="7 iron", command=sevenIron)
-button6 = Button(tk, text="8 iron", command=eightIron)
-button7 = Button(tk, text="9 iron", command=nineIron)
-button8 = Button(tk, text="Pitch", command=pitchingWedge)
-button9 = Button(tk, text="Sand", command=sandWedge)
-button10 = Button(tk, text="Reset Ball", command=resetBall)
+button2 = Button(tk, text="Wood", command=ThreeWood)
+button3 = Button(tk, text="5 iron", command=FiveIron)
+button4 = Button(tk, text="6 iron", command=SixIron)
+button5 = Button(tk, text="7 iron", command=SevenIron)
+button6 = Button(tk, text="8 iron", command=EightIron)
+button7 = Button(tk, text="9 iron", command=NineIron)
+button8 = Button(tk, text="Pitch", command=PitchingWedge)
+button9 = Button(tk, text="Sand", command=SandWedge)
+button10 = Button(tk, text="Reset Ball", command=ResetBall)
 
 #place buttons
 button.place(x=10, y=470)
@@ -181,7 +173,8 @@ button10.place(x=910, y=470)
 
 tk.bind("<Key-k>", lambda event:MoveBall())
 tk.bind("<Key-q>", lambda event:tk.destroy())
-tk.bind("<Key-r>", lambda event:resetBall())
+tk.bind("<Key-r>", lambda event:ResetBall())
+tk.bind("<Key-l>", lambda event:kollisjon())
 
 tk.mainloop()
 
